f84a8df3ce1caff1cfa7691d49a77fe74de72387
Heonbb <asfnkd@naver.com>
2024-05-03 16:47:27 +0900

    찐찐막

--- src/ContentPaneEx.java
+++ src/ContentPaneEx.java
@@ -3,7 +3,7 @@ import java.awt.*;
 
 public class ContentPaneEx extends JFrame {
     public ContentPaneEx() {
-        setTile("ContentPane과 JFrame 예제"); // 프레임의 타이틀 달기
+        setTitle("ContentPane과 JFrame 예제"); // 프레임의 타이틀 달기
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container contentPane = getContentPane(); // 컨텐트 팬 알아내기
@@ -16,7 +16,7 @@ public class ContentPaneEx extends JFrame {
         contentPane.add(new JButton("Ignore")); // Ignore 버튼 달기
 
         setSize(300, 150); // 프레임 크기 300x150 설정
-        setVisible(ture); // 화면에 프레임 출력
+        setVisible(true); // 화면에 프레임 출력
     }
 
     public static void main(String[] args) {
